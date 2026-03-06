3014fbd95da919bb6bbd5ed75a24313ba4334b44
Zach Vaughan <zach@zvaughan.com>
2026-03-06 09:59:55 -0700

    Update `LinkThis2Root.bat`

--- LinkThis2Root.bat
+++ LinkThis2Root.bat
@@ -2,9 +2,12 @@
 @REM Link2Root must be installed in "C:\Users\YOUR_USERNAME\AppData\Local\PowerShell Link2Root"
 @REM for this script to work properly.
 
-SET "InstallPath=%LOCALAPPDATA%/PowerShell Link2Root/Link2Root"
+SET "InstallPath=%LOCALAPPDATA%\PowerShell Link2Root"
+SET "ScriptPath=%InstallPath%\Link2Root.bat"
 
-IF NOT EXIST %InstallPath% (
+ECHO "%ScriptPath%"
+
+IF NOT EXIST "%ScriptPath%" (
     ECHO ERROR: Link2Root is not properly installed.
     ECHO.
     ECHO In order to use this script, you must run the Install-Link2Root installer
@@ -13,6 +16,6 @@ IF NOT EXIST %InstallPath% (
     GOTO :EndOfScript
 )
 
-CALL powershell -ExecutionPolicy Bypass -File "%LOCALAPPDATA%/PowerShell Link2Root/Link2Root" %*
+CALL "%ScriptPath%" %*
 
 :EndOfScript
