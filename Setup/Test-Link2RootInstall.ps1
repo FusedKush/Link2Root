1c5a576ef0d5e1ff0faca1a11243e49dc6cc4c8d
Zach Vaughan <zach@zvaughan.com>
2026-06-20 14:29:24 -0600

    Docs: Clean up `Test-Link2RootInstall.ps1`

--- Setup/Test-Link2RootInstall.ps1
+++ Setup/Test-Link2RootInstall.ps1
@@ -1,15 +1,29 @@
 <#
     .SYNOPSIS
-    Test if Link2Root is installed or not.
+    Test if Link2Root is currently installed.
 
     .DESCRIPTION
-    Test if Link2Root is currently installed on the computer.
+    Test if Link2Root is installed for the current user or not.
 
     By default, Link2Root will only be considered to be installed
     if it is present in the expected installation location, added
     to the current user's PowerShell Modules, and added to the user's PATH.
-    To specify which aspects of the installation to test,
-    use the `-TestInstall`, `-TestModule`, and `-TestPATH` switches.
+
+       - To specify which aspects of the installation to test,
+         use the `-TestInstall`, `-TestModule`, and `-TestPATH` switches.
+       
+       - To specify that the presence of *any* component should indicate that Link2Root
+         is currently installed, use the `-Any` switch.
+
+    By default, the progress and results of the installation will be printed
+    to the console, as well as visualized using a progress bar. To control this
+    behavior and disable output logging and/or progress bars, use the `-Silent`,
+    `-NoOutput`, and `-NoProgress` switches.
+
+       - `-Silent` is shorthand for both `-NoOutput` and `-NoProgress`.
+
+       - If you use the `-NoOutput` or `-Silent` switches, you must use the `-PassThru`
+         switch to be able to obtain any results from the function.
 
     .INPUTS
     None.
@@ -17,22 +31,47 @@
 
     .OUTPUTS
     Bool.
-    Returns `$true` if Link2Root is currently installed on the computer
-    or `$false` if it is not.
+    `Test-Link2RootInstall.ps1` returns `$true` if Link2Root is considered
+    to be installed for the current user or `$false` if it is not.
 
     .EXAMPLE
     .\Test-Link2RootInstall.ps1
-    False
+    [+] Link2Root Installed in C:\Users\FusedKush\AppData\Local\Link2Root
+    [-] Link2Root PowerShell Module NOT Installed in C:\Libraries\FusedKush\Documents\PowerShell\Modules\Link2Root
+    [+] Link2Root Added to My-PC\FusedKush's PATH
+    Link2Root is NOT Currently Installed!
+
+    Tests for the existence of the Link2Root Installation Files,
+    the Link2Root PowerShell Module, AND Link2Root in the User's PATH.
 
     .EXAMPLE
-    .\Test-Link2RootInstall.ps1 -TestModule
+    .\Test-Link2RootInstall.ps1
+    [+] Link2Root Installed in C:\Users\FusedKush\AppData\Local\Link2Root -Any
+    [-] Link2Root PowerShell Module NOT Installed in C:\Libraries\FusedKush\Documents\PowerShell\Modules\Link2Root
+    [+] Link2Root Added to My-PC\FusedKush's PATH
+    Link2Root is Currently Installed!
+
+    Tests for the existence of the Link2Root Installation Files,
+    the Link2Root PowerShell Module, OR Link2Root in the User's PATH.
+
+    .EXAMPLE
+    .\Test-Link2RootInstall.ps1 -TestModule -PassThru
+    [-] Link2Root PowerShell Module Installed in C:\Libraries\FusedKush\Documents\PowerShell\Modules\Link2Root
+    Link2Root is NOT Currently Installed!
+    False
+
+    Tests only for the existence of the Link2Root PowerShell Module while
+    also returning the result of the test.
+
+    .EXAMPLE
+    .\Test-Link2RootInstall.ps1 -TestInstall -TestPATH -Silent -PassThru
     True
 
-    .EXAMPLE
-    .\Test-Link2RootInstall.ps1 -TestInstall -TestPATH
-    False
+    Tests for the existence of both the Link2Root Installation Files
+    and Link2Root in the User's PATH and only returns the result of the test
+    without printing anything to the console.
 #>
-[CmdletBinding(DefaultParameterSetName = "WithOutput")]
+[CmdletBinding(DefaultParameterSetName = "WithOutput", PositionalBinding = $false)]
 param(
     <#
         Indicates that the existence of the
@@ -46,9 +85,9 @@ param(
         By default and when this switch is omitted, the integrity of the
         Link2Root Installation Directory and all of the files within will
         be validated and compared against the files in the parent directory
-        of this cmdlet.
+        of this script.
 
-        This switch is implied when invoking this cmdlet from within the
+        This switch is implied when invoking the script from within the
         Link2Root Installation Directory itself.
     #>
     [switch]$SkipInstallIntegrityCheck,
@@ -65,38 +104,102 @@ param(
         By default and when this switch is omitted, the integrity of the
         Link2Root PowerShell Module and all of the files within will
         be validated and compared against the files in the parent directory
-        of this cmdlet.
+        of this script.
 
-        This switch is implied when invoking this cmdlet from within the
+        This switch is implied when invoking the script from within the
         Link2Root Installation Directory.
     #>
     [switch]$SkipModuleIntegrityCheck,
     
     <#
-        Indicates that the existence of the
-        Link2Root Installation Directory in the
-        current user's PATH should be tested.
+        Indicates that the existence of the Link2Root Installation Directory
+        in the current user's PATH should be tested.
     #>
     [switch]$TestPATH,
 
+    <#
+        Indicates that the existence of any specified component should
+        reflect a valid Link2Root installation for the current user.
+        
+        By default and when this switch is omitted, ALL of the specified
+        components have to be present in order for Link2Root to be
+        considered to be installed for the current user.
+    #>
     [switch]$Any,
-
-    [Parameter(ParameterSetName = "WithoutOutputOrProgress", Mandatory)]
-    [switch]$Silent,
     
+    <#
+        Indicates that no output should be printed to the terminal
+        by the script.
+
+        By default and when neither this switch nor `-Silent` are used,
+        the status and results of the script are printed to the terminal.
+
+        If this switch is used, it MUST be combined with `-PassThru` in order
+        to obtain any results from this script.
+
+        Has no effect on progress bars or verbose output. To control the behavior
+        of Progress Bars, use the `-NoProgress` or `-Silent` switch.
+    #>
     [Parameter(ParameterSetName = "WithoutOutput", Mandatory)]
     [switch]$NoOutput,
 
+    <#
+        Indicates that no progress bars should be rendered
+        in the terminal by the script.
+
+        By default and when neither this switch nor `-Silent` are used,
+        the status and progress of the script is reflected in one or
+        more progress bars rendered within the terminal.
+
+        Has no effect on progress bars created by built-in functions and cmdlets.
+        To control the behavior of all PowerShell progress bars, use the
+        `$ProgressPreference` automatic variable.
+    #>
     [Alias("HideProgress")]
     [Parameter(ParameterSetName = "WithOutput")]
     [Parameter(ParameterSetName = "WithoutOutput")]
     [switch]$NoProgress,
 
+    <#
+        Indicates that no output or progress bars should be displayed
+        in the terminal by the script.
+
+        This switch is simply a shorthand for both `-NoOutput` and `-NoProgress`.
+
+        By default and when neither this switch, nor the `-NoOutput` and `-NoProgress`
+        switches, are used, the status, progress, and results of the script are
+        reflected in output and progress bars displayed in the terminal.
+
+        If this switch is used, it MUST be combined with `-PassThru` in order
+        to obtain any results from this script.
+
+        Has no effect on verbose output or progress bars created by built-in
+        functions and cmdlets. To control the behavior of all PowerShell progress bars,
+        use the `$ProgressPreference` automatic variable.
+    #>
+    [Parameter(ParameterSetName = "WithoutOutputOrProgress", Mandatory)]
+    [switch]$Silent,
+
+    <#
+        Indicates that the script should return the results of the
+        test as a boolean value.
+
+        By default and when this switch is omitted, this function
+        does not generate any output.
+
+        If the `-NoOutput` or `-Silent` switches are used, this switch
+        MUST be used in order to obtain any results from the script.
+    #>
     [Parameter(ParameterSetName = "WithOutput")]
     [Parameter(ParameterSetName = "WithoutOutput", Mandatory)]
     [Parameter(ParameterSetName = "WithoutOutputOrProgress", Mandatory)]
     [switch]$PassThru,
 
+    <#
+        An internal parameter used to flag that the script is being
+        invoked internally, which is used to optimize the behavior
+        of the script for internal calls.
+    #>
     [Parameter(DontShow)]
     [switch]$Internal,
 
@@ -108,43 +211,58 @@ param(
     [int]$Indentation = 0
 )
 
+Import-Module "$PSScriptRoot\Utils.psm1" -Verbose:($VerbosePreference -and -not $Internal)
 
+
+# Variables and Helper Functions #
+
+# The final result of the test, managed by `Update-TestResult`
+[bool]$result = !$Any
+# The location where Link2Root is supposed to be installed
+[string]$installLocation = & "$PSScriptRoot\Get-Link2RootInstall.ps1"
+
+<#
+    Update the final result of the test based
+    on the result of testing a specific component and
+    the presence or absence of the `-Any` switch to the script.
+#>
 function Update-TestResult {
     param(
+        <#
+            Indicates that the component test passed.
+        #>
         [Parameter(ParameterSetName = "SuccessfulResult")]
         [switch]$Success,
 
+        <#
+            Indicates that the component test failed.
+        #>
         [Parameter(ParameterSetName = "FailureResult")]
         [switch]$Failure
     )
 
-    if ($Failure -and -not $Any) {
-        $script:result = $false
-    }
-    elseif ($Success -and $Any) {
-        $script:result = $true
-    }
+    if ($Failure -and -not $Any) { $script:result = $false }
+    elseif ($Success -and $Any)  { $script:result = $true }
 }
 
 
-Import-Module "$PSScriptRoot\Utils.psm1" -Verbose:($VerbosePreference -and -not $Internal)
-
-[bool]$result = !$Any
-[string]$installLocation = & "$PSScriptRoot\Get-Link2RootInstall.ps1"
+# Main Script #
 
 if (-not ($TestInstall -or $TestModule -or $TestPATH)) {
     $TestInstall = $TestModule = $TestPATH = $true
 }
 if ($Silent) {
-    $NoOutput = $true
-    $NoProgress = $true
+    $NoOutput = $NoProgress = $true
 }
 
-if (-not $NoProgress)   { Enable-ProgressBars }
-else                    { Disable-ProgressBars }
+if (-not $NoProgress) { Enable-ProgressBars }
+else                  { Disable-ProgressBars }
 
 Write-Verbose "$(Get-IndentString $Indentation)[>] Checking Current Installation Status of Link2Root..."
 Add-ProgressBar -Name "Checking Link2Root Installation Status" -DefaultPercentageChange 40 -InitialSecondsRemaining 3
+
+
+# Check for the presence and validity of the Link2Root Installation Files
 Update-ProgressBar -Status "Check Install Status" -CurrentOperation "Checking Status..." -PercentageChange 0
 
 if ($TestInstall) {
@@ -200,7 +318,6 @@ if ($TestInstall) {
                 Write-Host " in " -NoNewline
                 _wp $installLocation
             }
-
         }
     }
     else {
@@ -224,12 +341,18 @@ else {
 }
 
 
+# Check for the presence and validity of the Link2Root PowerShell Module
 Update-ProgressBar -Status "Check PowerShell Module Status" -CurrentOperation "Checking Status..." -PercentageChange 0
 
 if ($TestModule) {
     Write-Verbose "$(Get-IndentString ($Indentation + 1))[>] Checking Current PowerShell Module Status..."
     
-    [string]$modulePath = (& "$PSScriptRoot\Get-Link2RootInstall.ps1" -GetModulePath -Internal:$Internal -Indentation ($Indentation + 2))
+    [string]$modulePath = (
+        & "$PSScriptRoot\Get-Link2RootInstall.ps1" `
+            -GetModulePath `
+            -Internal:$Internal `
+            -Indentation ($Indentation + 2)
+    )
 
     if (Test-Path $modulePath -Type Container) {
         [hashtable]$integrityCheckArgs = @{
@@ -296,6 +419,7 @@ else {
 }
 
 
+# Check for the presence of Link2Root in the User's PATH
 Update-ProgressBar -Status "Check PATH Status" -CurrentOperation "Checking Status..." -PercentageChange 0
 
 if ($TestPATH) {
@@ -304,7 +428,6 @@ if ($TestPATH) {
     Write-Verbose "$(Get-IndentString ($Indentation + 1))[>] Checking Current PATH..."
     
     if (Test-UserPATH $installLocation -Indentation ($Indentation + 2) -Verbose:$VerbosePreference) {
-        # Write-Verbose "$(Get-IndentString ($Indentation + 2))[+] Entry $installLocation FOUND in $username's PATH"
         Write-Verbose "$(Get-IndentString ($Indentation + 1))[+] Current PATH Status: FOUND"
         Update-ProgressBar -Status "Check PATH Status" -CurrentOperation "Component Installed!"
         Update-TestResult -Success
@@ -338,25 +461,18 @@ else {
 }
 
 
+# Cleanup and print and/or return the results
 Remove-ProgressBar
 
-if ($result) {
-    Write-Verbose "$(Get-IndentString $Indentation)[+] Link2Root IS considered to be installed"
-}
-else {
-    Write-Verbose "$(Get-IndentString $Indentation)[-] Link2Root is NOT considered to be installed"
-}
+if ($result) { Write-Verbose "$(Get-IndentString $Indentation)[+] Link2Root IS considered to be installed" }
+else         { Write-Verbose "$(Get-IndentString $Indentation)[-] Link2Root is NOT considered to be installed" }
 
 if (-not $NoOutput) {
     _wc "Link2Root" -NoNewline
     Write-Host " is " -NoNewline
 
-    if ($result) {
-        Write-Host "Currently Installed!" -ForegroundColor Green
-    }
-    else {
-        Write-Host "NOT Currently Installed!" -ForegroundColor Red
-    }
+    if ($result) { Write-Host "Currently Installed!" -ForegroundColor Green }
+    else         { Write-Host "NOT Currently Installed!" -ForegroundColor Red }
 }
 
 if ($PassThru) {
